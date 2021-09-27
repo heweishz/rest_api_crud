6db4e4d811b0b5e1900b02cd5ead84534ec2ab17
william ho <heweishz@gmail.com>
2021-09-28 06:19:54 +0800

    init

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+require('dotenv/config')
+app.use(cors())
+app.use(bodyParser.json())
+
+const router = require('./routes/posts.js')
+app.use('/', (req, res, next) => {
+  console.log('hello remote friend')
+  next()
+})
+
+app.use('/posts', router)
+app.use('/posts/specific', (req, res) => {
+  res.send('can i show?')
+})
+//connect to db
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () =>
+  console.log('DB connected')
+)
+
+app.listen(3000)
